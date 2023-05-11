3d9b2cb80cacf9b5de129e1f7a8fbfa791da6103
Mcklmo <87199710+Mcklmo@users.noreply.github.com>
2023-05-11 11:56:12 +0200

    [fix] element list type visitor populates all nested lists with type nodes

--- out.cs
+++ out.cs
@@ -2,11 +2,8 @@ using System.Collections.Generic;
 using System;
 
 class Program() {
-	static public List<List<double>> testFunc(double a,bool b){
-		a = 2.0;
-	}
+	List<List<double>> a = new List<double>{new List<double>{}()}();
 	static public void Main(List<String> args){
-		testFunc(1.0,true);
 	}
 
 }
--- src/tests.py
+++ src/tests.py
@@ -1,14 +1,28 @@
 from tests.test_ast_single_dispatch_visitor import TestASTSingleDispatchVisitor
+from tests.test_type_check_visitor import TestTypeCheckVisitor
+visitors = [
+    TestASTSingleDispatchVisitor(),
+    TestTypeCheckVisitor()
+]
 
-test_ast_single_dispatch_visitor = TestASTSingleDispatchVisitor()
 
-test_ast_single_dispatch_visitor.setUp()
-i=0
-print("Running tests...\n")
-for method_name in dir(test_ast_single_dispatch_visitor) :
-    if method_name.startswith("test_"):
-        test_method = getattr(test_ast_single_dispatch_visitor, method_name)
-        test_method()
-        print(f"{method_name}")
-        i+=1
-print(f"\n{i} tests ok")
+def run_tests(visitors):
+    i = 0
+    for visitor in visitors:
+        visitor.setUp()
+
+        print(str(type(visitor).__name__))
+
+        for method_name in dir(visitor):
+            if method_name.startswith("test_"):
+                test_method = getattr(visitor, method_name)
+                test_method()
+                print(f"    {method_name}")
+                i += 1
+
+        visitor.tearDown()
+
+    print(f"\n{i} tests ok")
+
+
+run_tests(visitors)
